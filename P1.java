99b209dce724ca1e57f729f4084eff9cb448288a
GITAdmin <damodarrhce@gmail.com>
2019-02-04 19:16:38 +0530

    Modified data

---
+++ P1.java
@@ -0,0 +1,16 @@
+class P1
+{
+
+   void display()
+   {
+	System.out.println("  Hllo.... ");
+	System.out.println(" Another line added ...|);
+   }
+
+
+public static void main(String args[])
+{
+  P1 obj = new P1();
+  obj.display();
+}
+}
